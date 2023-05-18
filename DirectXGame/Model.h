428fcd3a398d2e25987160ad72979de77e33e9b5
etigoshun <k021g1375@g.neec.ac.jp>
2023-05-18 12:41:26 +0900

    メッシュの読み込み

--- DirectXGame/Model.h
+++ DirectXGame/Model.h
@@ -3,27 +3,39 @@
 #include <string>
 #include <DirectXMath.h>
 #include <vector>
+#include <DirectXTex.h>
+
+using namespace DirectX;
 
 struct Node
 {
 	//名前
 	std::string name;
 	//ローカルスケール
-	DirectX::XMVECTOR scaling = { 1,1,1,0 };
+	XMVECTOR scaling = { 1,1,1,0 };
 	//ローカル回転角
-	DirectX::XMVECTOR rotation = { 0,0,0,0 };
+	XMVECTOR rotation = { 0,0,0,0 };
 	//ローカル移動
-	DirectX::XMVECTOR translation = { 0,0,0,1 };
+	XMVECTOR translation = { 0,0,0,1 };
 	//ローカル変形行列
-	DirectX::XMMATRIX transform;
+	XMMATRIX transform;
 	//グローバル変形行列
-	DirectX::XMMATRIX gloabalTransform;
+	XMMATRIX gloabalTransform;
 	//親ノード
 	Node* parent = nullptr;
 };
 
 class Model
 {
+public:	//サブクラス
+	//頂点データ構造体
+	struct VertexPosNormalUv
+	{
+		XMFLOAT3 pos;		//xyz座標
+		XMFLOAT3 normal;	//法線ベクトル
+		XMFLOAT2 uv;		//uv座標
+	};
+
 public:
 	//フレンドクラス
 	friend class FbxLoader;
@@ -33,5 +45,19 @@ private:
 	std::string name;
 	//ノード配列
 	std::vector<Node> nodes;
+	//メッシュを持つノード
+	Node* meshNode = nullptr;
+	//頂点データ配列
+	std::vector<VertexPosNormalUv> vertices;
+	//頂点インデックス配列
+	std::vector<unsigned short> indices;
+	//アンビエント係数
+	XMFLOAT3 ambient = { 1,1,1 };
+	//ディフューズ係数
+	XMFLOAT3 diffuse = { 1,1,1 };
+	//テクスチャメタデータ
+	TexMetadata metadata = {};
+	//スクラッチイメージ
+	ScratchImage scratchImg = {};
 };
 
