42bca8791437f9086d769ad40f161f4966a6c5ba
etigoshun <k021g1375@g.neec.ac.jp>
2023-05-24 15:19:22 +0900

    FBXモデルの描画(モデル)

--- DirectXGame/3d/FbxLoader.cpp
+++ DirectXGame/3d/FbxLoader.cpp
@@ -70,6 +70,9 @@ void FbxLoader::LoadModelFromFile(const string& modelName)
 	ParseNodeRecursive(model, fbxScene->GetRootNode());
 	//FBXシーン解放
 	fbxScene->Destroy();
+
+	//バッファ生成
+	model->CreateBuffers(device);
 }
 
 void FbxLoader::ParseNodeRecursive(Model* model, FbxNode* fbxNode, Node* parent)
