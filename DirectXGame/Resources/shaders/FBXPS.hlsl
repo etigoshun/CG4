8b03bdf9ceed5c6b81b012f5da8f72fcce2baab1
etigoshun <k021g1375@g.neec.ac.jp>
2023-07-12 16:35:48 +0900

    マルチレンダーターゲット

--- DirectXGame/Resources/shaders/FBXPS.hlsl
+++ DirectXGame/Resources/shaders/FBXPS.hlsl
@@ -5,9 +5,17 @@ Texture2D<float4> tex : register(t0);
 //0番スロットに設定されたサンプラー
 SamplerState smp : register(s0);
 
-//エントリーポイント
-float4 main(VSOutput input) : SV_TARGET
+struct PSOutput
 {
+	float4 target0 : SV_TARGET0;
+	float4 target1 : SV_TARGET1;
+};
+
+//エントリーポイント
+PSOutput main(VSOutput input) : SV_TARGET
+{
+	PSOutput output;
+
 	//テクスチャマッピング
 	float4 texcolor = tex.Sample(smp,input.uv);
 
@@ -18,5 +26,7 @@ float4 main(VSOutput input) : SV_TARGET
 	float4 shadercolor = float4(brightness, brightness, brightness, 1.0f);
 
 	//陰影とテクスチャの色を合成
-	return shadercolor * texcolor;
+	output.target0 = shadercolor * texcolor;
+	output.target1 = float4(1 - (shadercolor * texcolor).rgb, 1);
+	return output;
 }
