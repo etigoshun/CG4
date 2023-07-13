d77e69d208ca7cbb1b7938357e45b979a0e95387
etigoshun <k021g1375@g.neec.ac.jp>
2023-07-13 15:11:55 +0900

    評価課題完了

--- DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -8,12 +8,37 @@ float4 main(VSOutput input) : SV_TARGET
 {
 	float4 colortex0 = tex0.Sample(smp, input.uv);
 	float4 colortex1 = tex1.Sample(smp, input.uv);
+	float4 blur = 0.0f;
 
-	float4 color = colortex0;
+	//テクスチャのサイズを指定
+	int uWidth = 512;  // テクスチャの幅
+	int vHeight = 512; // テクスチャの高さ
+
+	// サンプリング範囲の設定
+	float2 texelSize = float2(1.0 / uWidth, 1.0 / vHeight);
+
+	// カーネルサイズ
+	int kernelSize = 1;
+
+	// サンプリング範囲内のピクセルを合計
+	for (int x = -kernelSize; x <= kernelSize; x++)
+	{
+		for (int y = -kernelSize; y <= kernelSize; y++)
+		{
+			float2 offset = float2(x, y) * texelSize;
+			blur += tex1.Sample(smp, input.uv + offset);
+		}
+	}
+
+	// ピクセル数で割って平均化
+	//blur /= (kernelSize * 2 + 1) * (kernelSize * 2 + 1);
+	colortex1.rgb = blur / 9.0f;
+
+	float4 color = 1.0f - colortex0;
 
 	if (fmod(input.uv.y, 0.1f) < 0.05f)
 	{
-		color = colortex1;
+		color = lerp(colortex0, colortex1, 0.4f);
 	}
 
 	return float4(color.rgb, 1);
